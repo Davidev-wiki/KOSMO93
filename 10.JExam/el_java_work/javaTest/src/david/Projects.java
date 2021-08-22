d8ac15192b0080ecf766cafb5c284cf03c6f39b1
Davidev <accelerator@kakao.com>
2021-08-23 00:55:39 +0900

    add package

---
+++ 10.JExam/el_java_work/javaTest/src/david/Projects.java
@@ -0,0 +1,15 @@
+package david;
+
+public class Projects {
+
+	private String pjName = "00 프로젝트";
+	private int pjPeriod = 20;
+
+	public static void main(String[] args) {
+
+		Projects pj = new Projects();
+		System.out.println("프로젝트 이름 : " + pj.pjName);
+		System.out.println("프로젝트 기간 : 총 " + pj.pjPeriod + "일");
+
+	}
+}
