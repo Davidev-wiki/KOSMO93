12ccf24531ba1e72b486b1b4ce027b2a74cc1be3
Davidev <accelerator@kakao.com>
2021-08-28 19:08:54 +0900

    add workspace Neon

---
+++ 10.JExam/mini_Project/javaKck/src/com/kosmo/kck/login/KckLogin.java
@@ -0,0 +1,10 @@
+package com.kosmo.kck.login;
+
+public class KckLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 10.JExam/mini_Project/javaKck/src/com/kosmo/kck/login/KckLoginService.java
@@ -0,0 +1,5 @@
+package com.kosmo.kck.login;
+
+public interface KckLoginService {
+
+}
